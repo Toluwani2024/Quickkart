53c672fd486b6e2711727273b71fd09cb9db5ce8
Toluwani <aderibigbeadewunmi204@gmail.com>
2025-05-01 17:45:47 +0100

    Fix case-sensitive import for Cart.css

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './cart.css';
+import './Cart.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeItem } from "../../redux/cartSlice";
 
